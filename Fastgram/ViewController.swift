b1c1ddd3dbcd0b7ae20258d540c6407f2ffc2872
Bepo0328 <82445568+Bepo0328@users.noreply.github.com>
2022-02-16 22:10:43 +0900

    Initial commit

---
+++ Fastgram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fastgram
+//
+//  Created by 전윤현 on 2022/02/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
